5fb5ca544cb5b1656fbba32a8aaaec9d1fecf2fe
HugoZAuad <hugozeymer@hotmail.com>
2025-09-25 16:36:54 -0300

    Fix: Aajustando o fluxo de contato pelo formulario

--- src/contact/services/contact.service.ts
+++ src/contact/services/contact.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import { ContactDto } from '../DTO/contact.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ContactService {
+  constructor(private configService: ConfigService) {}
+
   async sendEmail(contact: ContactDto): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: 'hugozeymer@gmail.com',
-        pass: 'frtu tjtm vkez hggf',
+        user: process.env.ADMIN_EMAIL || 'hugozeymer@gmail.com',
+        pass: process.env.PASS_GMAIL || 'SECRETPASSWORD_MUDAR',
       },
     });
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     await transporter.sendMail({
       from: contact.email,
-      to: 'hugozeymer@gmail.com',
+      to: process.env.ADMIN_EMAIL,
       subject: `Nova mensagem de ${contact.name}`,
       text: contact.message,
     });
