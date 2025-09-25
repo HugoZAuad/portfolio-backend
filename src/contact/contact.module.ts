35b403441c2a6f32fa16f0e5317ceb10ec0fe700
HugoZAuad <hugozeymer@hotmail.com>
2025-09-25 16:47:46 -0300

    Fix: Ajuste de module import

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ContactController } from './controllers/contact.controller';
 import { ContactService } from './services/contact.service';
+import { ConfigModule } from '@nestjs/config'; // Importe o ConfigModule
 
 @Module({
+  imports: [ConfigModule],
   controllers: [ContactController],
   providers: [ContactService],
 })
